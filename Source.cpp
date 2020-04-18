cfb1e2ea819b8aaa55cab0cbcc9b78835fe9a661
Mohi-mo <61887500+Mohi-mo@users.noreply.github.com>
2020-04-19 00:04:33 +0300

    Add files via upload
    //task 1(d) 2(a,b) (18/04/2020). Generate an array with random numbers in the range [-70;50]. Create a program that will count the number of shuffling actions. Analyze the dependence of the number of permutations on the number of elements in an array

--- Source.cpp
+++ Source.cpp
@@ -3,61 +3,101 @@
 #include <iomanip>
 #include <math.h>
 
-//задача 5 (11/04/2020). Найти наибольший/наименьший элемент массива и его номер по порядку.
+//task 1(d) 2(a,b) (18/04/2020). Generate an array with random numbers in the range [-70;50].
 
-const int n = 20;
-int array[n];
-int numbmin, numbmax, min, max;
+void insertionSort(int array[], int n) {
+	
+	float key = 0;
+	int j, iterIsort = 0;
 
-void Filework() {
+	for (int i = 1; i < n; i++){
+		
+		key = array[i]; 
+		j = i - 1;
+		iterIsort += 1;
+
+		while (j >= 0 && array[j] > key){
+			
+			array[j + 1] = array[j];
+			j = j - 1;
+			array[j + 1] = key;
+		}
+		
+	}
+	
+	std::cout << std::endl;
+	std::cout << "Number of permutations: " << iterIsort << std::endl;
+
+}
+
+void selelctSort(int array[], int n) {
+	int j = 0;
+	int passing = 0;
+	int iterPermutationsSort = 0;
+	
+	for (int i = 0; i < n; i++){
+		
+		j = i;
+
+		for (int k = i; k < n; k++){
+
+			if (array[j] > array[k]){
+
+				j = k;
+				iterPermutationsSort += 1;
+
+			}
+		}
+
+		passing = array[i];
+		array[i] = array[j];
+		array[j] = passing;
+
+	}
+
+	std::cout<<std::endl;
+	std::cout << "Number of permutations: " << iterPermutationsSort << std::endl;
+
+}
+
+void fileWork() {
 	std::ofstream file;
 	file.open("result.txt");
-
-	file << "Max element: " << max << std::endl;
-	file << "Min element: " << min << std::endl;
-
-	file << "Number maximum: " << numbmax << std::endl;
-	file << "Number minimum: " << numbmin << std::endl;
-
 	file.close();
 }
 
 int main() {
 	
-	srand(time(NULL));
+	const int n = 1200;
+	int array[n];
 	
-	min = array[0];
-	max = array[0];
+	srand(100);
+	
+	for (int i = 0; i < n; i++){
 
-	for (int i = 0; i < n; i++) {
+		array[i] = rand() % 121 - 70;
+		std::cout << array[i] << " ";
 
-		array[i] = rand() % 20 - 10;
-		std::cout << array[i] << "; ";
-		//file << array[i] << "; ";
+	}
+	
+	//selelctSort(array, n); //
 
-		if (array[i]>max){
-			max = array[i];
-			numbmax = i;
-		}
+	insertionSort(array, n); // 
+	
+	std::cout << std::endl << std::endl;
+	
+	for (int i = 0; i < n; i++){
 
-		if (array[i] < min) {
-			min = array[i];
-			numbmin = i;
-		}
+		std::cout << array[i] << " ";
+
 	}
 
-	std::cout << std::endl;
-	std::cout<<"Max element: " << max << std::endl;
-	std::cout <<"Min element: "<< min << std::endl;
-	
-	std::cout<<std::endl;
-	std::cout << "Number maximum: " << numbmax << std::endl;
-	std::cout << "Number minimum: " << numbmin << std::endl;
-	
-	Filework();
+	//fileWork();
+
 
 	getchar();
 	getchar();
 	
 	return 0;
 }
+/* при выполнении заметил, что сортировка вставками быстрее подбором, разница была примерно 256 перестановок*/
